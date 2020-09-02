7660038651f2b3e36202df4a5aab5e36ca54e2ff
JavaPhish <cclements3150@gmail.com>
2020-09-01 21:55:32 -0400

    Route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Basic flask hello world """
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ The hello world thing """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbtn():
+    """ different directory """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_str(text):
+    """ text variable """
+    parse = "C "
+    parse = parse + text.replace("_", " ")
+    return parse
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def hello_str2(text='is cool'):
+    """ text variable """
+    parse = "Python "
+    parse = parse + text.replace("_", " ")
+    return parse
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
