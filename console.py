ffd5516fca0e4f35d1a292253e9b7ab06f05c0b0
JavaPhish <cclements3150@gmail.com>
2020-09-02 19:20:11 -0400

    Fixed syntax thing

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -127,10 +127,10 @@ class HBNBCommand(cmd.Cmd):
         new_instance = HBNBCommand.classes[parts[0]]()
         if len(parts) > 1:
             for param in parts[1:]:
-                if "=" not in param or param[0] is "=" or param[-1] is "=":
+                if "=" not in param or param[0] == "=" or param[-1] == "=":
                     continue
                 params = param.split("=")
-                if params[1][0] is "\"":
+                if params[1][0] == "\"":
                     params[1] = params[1].replace("_", " ")
                     setattr(new_instance, params[0], params[1])
                     continue
@@ -288,7 +288,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -296,10 +296,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
